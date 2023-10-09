c542ab84cb571f3a86c2bf56a2cad5cab1643df1
Anastasia <nastusya7cat7@gmail.com>
2023-10-09 22:44:40 +0300

    Добавили Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите элементы массива через пробел: ");
+int[] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{    
+    if(array[i] > 0)
+        count ++;
+}
+Console.WriteLine($"Количество элементов > 0: {count}");
